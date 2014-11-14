9f0da2c485afe69a6a6563c0d8a607938fdae450
Anthony Braddick <acb10@stmarys-ca.edu>
2014-11-14 01:20:41 -0800

    Testing candy crush clone example

---
+++ NimGame/Cookie.swift
@@ -0,0 +1,50 @@
+//
+//  Cookie.swift
+//  NimGame
+//
+//  Created by Anthony Braddick on 11/14/14.
+//  Copyright (c) 2014 Anthony Braddick. All rights reserved.
+//
+
+import Foundation
+import SpriteKit
+
+enum CookieType: Int, Printable {
+    case Unknown = 0, Croissant, Cupcake, Danish, Donut, Macaroon, SugarCookie
+    
+    var spriteName: String {
+        let spriteNames = [
+            "Croissant",
+            "Cupcake",
+            "Danish",
+            "Donut",
+            "Macaroon",
+            "SugarCookie"]
+        
+        return spriteNames[rawValue - 1]
+    }
+    
+    var highlightedSpriteName: String {
+        return spriteName + "-Highlighted"
+    }
+    
+    static func random() -> CookieType {
+        return CookieType(rawValue: Int(arc4random_uniform(6)) + 1)!
+    }
+}
+
+class Cookie: Printable {
+    var column: Int
+    var row: Int
+    let cookieType: CookieType
+    var sprite: SKSpriteNode?
+    
+    init(column: Int, row: Int, cookieType: CookieType) {
+        self.column = column
+        self.row = row
+        self.cookieType = cookieType
+    }
+    var description: String {
+        return "type:\(cookieType) square:(\(column),\(row))"
+    }
+}
